870a74e71141c3962402c832427001407ba95f3f
y0av <mail.yoav@gmail.com>
2025-01-16 12:23:59 +0200

    all basic parts are working

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,10 @@ const logger = require("firebase-functions/logger");
 const { onSchedule } = require("firebase-functions/v2/scheduler");
 require("dotenv").config();
 const OpenAI = require("openai");
-const axios = require("axios");
+const fs = require("fs");
+const git = require("simple-git");
+const path = require("path");
+const os = require("os");
 
 
 exports.info = onRequest((request, response) => {
@@ -48,4 +51,84 @@ async function getPostContent(articleContent) {
     }
 }
 
+/**
+ * Creates a Markdown file with the given content
+ * @async
+ * @param {string} folderName - The name of the repo folder
+ * @param {string} fileName - The name of the file to create
+ * @param {string} content - The content to write to the file
+ * @return {Promise<void>}
+ */
+async function createMdFile(folderName, fileName, content) {
+    try {
+        const tempFilePath = path.join(os.tmpdir(), folderName, fileName);
+        console.log({ tempFilePath });
+        fs.writeFileSync(tempFilePath, content, "utf8");
+        return tempFilePath;
+    } catch (error) {
+        console.error("Error creating file:", error);
+        throw error;
+    }
+}
+
+/**
+ * Clones a GitHub repository to a local temporary directory.
+ * @async
+ * @param {string} userName - The name of the repository owner
+ * @param {string} repoName - The name of the repository
+ * @return {Promise<string>} The remote path to the cloned repository
+ */
+async function cloneRepo(userName, repoName) {
+    // const USER = "y0av";
+    const TOKEN = process.env.GITHUB_TOKEN;
+    const remote = `https://${TOKEN}@github.com/${userName}/${repoName}`;
+    const localPath = path.join(os.tmpdir(), repoName);
+
+    try {
+        await git().clone(remote, localPath);
+        return remote;
+    } catch (error) {
+        console.error("Error cloning repository:", error);
+        throw error;
+    }
+}
+
+/**
+ * Commits the specified file to a GitHub repository.
+ * @async
+ * @param {string} repoName - The name of the repository 
+ * @param {string} fileName - The name of the file to commit
+ * @return {Promise<number>} 200 if successful, otherwise if there is an error
+ */
+async function commitFile(repoName, fileName) {
+    try {
+        const localRepoPath = path.join(os.tmpdir(), repoName);
+        const filePath = path.join(localRepoPath, fileName);
+
+        await git(localRepoPath)
+            .add(filePath)
+            .commit("Add new blog post file")
+            .push("origin", "main");
+        console.log("File committed and pushed to GitHub repository");
+        return 200;
+    } catch (error) {
+        console.error("Error committing file:", error);
+        return 400;
+    }
+}
+
+/**
+ * test function
+ * @async
+ */
+async function test() {
+    console.log("starting test function");
+    const cloned = await cloneRepo("y0av", "blog.berry");
+    console.log("cloned repo:", cloned);
+    const f = await createMdFile("blog.berry", "test.md", "Hello, file!");
+    console.log("created file:", f);
+    await commitFile("blog.berry", "test.md");
+    console.log("done test function");
+}
+test();
 // getPostContent();
